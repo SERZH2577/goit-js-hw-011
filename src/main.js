280430755f1b5ee9ccd485e636fee68785796158
Serhii Plis <serzh2577@gmail.com>
2023-12-30 23:06:35 +0300

    start

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+//   https://pixabay.com/api/
+
+//   "Sorry, there are no images matching your search query. Please try again!"
+
+//   [refresh()](https://github.com/andreknieriem/simplelightbox#public-methods)
